960ea86868bc80f8ef8f5f99d4a56f7e17b72cc9
Zac <a19860101@gmail.com>
2022-06-14 19:52:29 +0800

    20220614

---
+++ students/db.php
@@ -0,0 +1,9 @@
+<?php
+    $db_host = 'localhost';
+    $db_user = 'admin';
+    $db_pw = 'admin';
+    $db_name = 'php-24-20220607';
+
+    $db = new mysqli($db_host,$db_user,$db_pw,$db_name);
+
+    
