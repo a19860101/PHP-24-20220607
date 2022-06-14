a50b2130aa0b90cc2522ba3d3786dae3036d48fc
Zac <a19860101@gmail.com>
2022-06-14 20:04:14 +0800

    20220614

--- students/db.php
+++ students/db.php
@@ -4,6 +4,19 @@
     $db_pw = 'admin';
     $db_name = 'php-24-20220607';
 
-    $db = new mysqli($db_host,$db_user,$db_pw,$db_name);
+    mysqli_report(MYSQLI_REPORT_OFF);
 
-    
+    // Procedural
+    // $db = @mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('database error');
+
+    // mysqli_query($db,'SET NAMES utf8mb4');
+
+    //Object-oriented
+
+    $db = @new mysqli($db_host,$db_user,$db_pw,$db_name);
+
+    if($db->connect_errno){
+        echo $db->connect_error;
+    }
+
+    $db->query('SET NAMES utf8mb4');
