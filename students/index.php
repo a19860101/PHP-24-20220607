ac45bd1d67d895847c10b612c3c619df10c4b772
Zac <a19860101@gmail.com>
2022-06-14 20:17:56 +0800

    20220614

--- students/index.php
+++ students/index.php
@@ -1,2 +1,21 @@
 <?php
     include('db.php');
+    $sql = 'SELECT * FROM students';
+    $result = mysqli_query($db,$sql);
+    $data = mysqli_fetch_assoc($result);
+    $datas = mysqli_fetch_all($result);
+
+    var_dump($datas);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
