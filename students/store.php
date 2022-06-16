dedc9813342203b2322ab6e2ce165e03e4e22f22
Zac <a19860101@gmail.com>
2022-06-16 21:03:15 +0800

    20220616

--- students/store.php
+++ students/store.php
@@ -3,9 +3,13 @@
     extract($_REQUEST);
     $skill = implode(',',$skill);
 
-    $sql = "INSERT INTO students(username,pw,birth,edu,gender,skill,content)
-            VALUES('$username','$pw','$birth','$edu','$gender','$skill','$content')";
+//     $sql = "INSERT INTO students(username,pw,birth,edu,gender,skill,content)
+//             VALUES('$username','$pw','$birth','$edu','$gender','$skill','$content')";
+//     mysqli_query($db,$sql);
 
-    mysqli_query($db,$sql);
+    $sql = 'INSERT INTO students(username,pw,birth,edu,gender,skill,content)VALUES(?,?,?,?,?,?,?)';
+	$stmt = $db->prepare($sql);
+	$stmt->bind_param('sssssss',$username,$pw,$birth,$edu,$gender,$skill,$content);
+	$stmt->execute();
 
-    header('location:index.php');
+	header('location:index.php');
