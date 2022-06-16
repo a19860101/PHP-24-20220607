214c18ee523dd46aedb29a2b6f22dd98c640275a
Zac <a19860101@gmail.com>
2022-06-16 21:26:44 +0800

    20220616

--- students/update.php
+++ students/update.php
@@ -8,17 +8,22 @@
         $skill = implode(',',$skill);
     }
 
-    $sql = "UPDATE students SET
-            username = '$username',
-            pw = '$pw',
-            birth = '$birth',
-            edu = '$edu',
-            gender = '$gender',
-            skill = '$skill',
-            content = '$content'
-            WHERE id = {$id}
-    ";
+    // $sql = "UPDATE students SET
+    //         username = '$username',
+    //         pw = '$pw',
+    //         birth = '$birth',
+    //         edu = '$edu',
+    //         gender = '$gender',
+    //         skill = '$skill',
+    //         content = '$content'
+    //         WHERE id = {$id}
+    // ";
 
-    mysqli_query($db, $sql);
+    // mysqli_query($db, $sql);
+
+    $sql = 'UPDATE students SET username=?,pw=?,birth=?,gender=?,edu=?,skill=?,content=? WHERE id =?';
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param('sssssssi',$username,$pw,$birth,$gender,$edu,$skill,$content,$id);
+    $stmt->execute();
 
     header('location:index.php');
