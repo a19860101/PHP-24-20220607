4c7bda2f41bf2b98f08d6b1f02c775c508398330
Zac <a19860101@gmail.com>
2022-06-21 21:22:27 +0800

    20220621

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -1,2 +1,21 @@
 <?php
-    var_dump($_FILES);
+    // var_dump($_FILES);
+    // $name = $_FILES['img']['name'];
+    // $size = $_FILES['img']['size'];
+    // $tmp_name = $_FILES['img']['tmp_name'];
+    // $error = $_FILES['img']['error'];
+    // $full_path = $_FILES['img']['full_path'];
+    // $type = $_FILES['img']['type'];
+    extract($_FILES['img']);
+    echo $name;
+    echo '<br>';
+    echo $size;
+    echo '<br>';
+    echo $tmp_name;
+    echo '<br>';
+    echo $error;
+    echo '<br>';
+    echo $full_path;
+    echo '<br>';
+    echo $type;
+    echo '<br>';
