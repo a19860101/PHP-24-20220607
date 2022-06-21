69eb4eeba337629b165d18b7533eec92df05ac2d
Zac <a19860101@gmail.com>
2022-06-21 21:48:51 +0800

    20220621

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -20,14 +20,15 @@
     // echo '<br>';
     extract($_FILES['img']);
 
-    $img_name = md5(time());
-    $ext = pathinfo($name,PATHINFO_EXTENSION);
-    $fullname = $img_name.'.'.$ext;
+    $img_name = md5(time()); //建立亂數檔名
+    $ext = pathinfo($name,PATHINFO_EXTENSION); //取得副檔名
+    $fullname = $img_name.'.'.$ext; //合併檔名+副檔名
 
-    $folder = 'images/';
-    $target = $folder.$fullname;
+    $folder = 'images/'; //上傳路徑
+    $target = $folder.$fullname; // 目標路徑
     if(!is_dir($folder)){
         mkdir($folder);
+        //若資料夾不存在就建立資料夾
     }
     if($error == 0){
         move_uploaded_file($tmp_name,$target);
