3373091d81784bc108fba6412f3f435fb10ba1f7
Zac <a19860101@gmail.com>
2022-06-23 19:18:46 +0800

    20220623

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -29,6 +29,7 @@
         echo '<script>alert("請上傳正確的圖片格式")</script>';
         header('refresh:0;url=form.php');
         return ;
+        // 終止下面的動作
     }
 
 
@@ -38,12 +39,35 @@
         mkdir($folder);
         //若資料夾不存在就建立資料夾
     }
-    if($error == 0){
-        if(move_uploaded_file($tmp_name,$target)){
-            echo '<script>alert("上傳成功")</script>';
-        }else{
-            echo '<script>alert("上傳失敗")</script>';
-        }
+    switch($error){
+        case 0:
+            if(move_uploaded_file($tmp_name,$target)){
+                echo '<script>alert("上傳成功")</script>';
+            }else{
+                echo '<script>alert("上傳失敗")</script>';
+            }
+            break;
+        case 1:
+            echo '上傳檔案過大(伺服器限制)';
+            break;
+        case 2:
+            echo '上傳檔案過大(表單限制)';
+            break;
+        case 3:
+            echo '只有部分上傳';
+            break;
+        case 4:
+            echo '請選擇檔案';
+            break;
+        case 6:
+            echo '遺失暫存資料夾';
+            break;
+        case 7:
+            echo '無法寫入';
+            break;
+        case 8:
+            echo '不支援檔案上傳';
+            break;
         
     }
 
