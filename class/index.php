75738df863ef3a844072063de7cd92f3c015c00c
Zac <a19860101@gmail.com>
2022-06-30 19:36:21 +0800

    20220630

--- class/index.php
+++ class/index.php
@@ -3,15 +3,22 @@
         // 屬性
         public $name = 'John Doe';
         public $gender;
+
+        // 方法
+        public function walk(){
+            echo $this->name.'walk!!!';
+            return 'walk!';
+        }
     }
 
     $john = new Role; // 建立實體(物件)
     $john->name = 'John';
     $john->gender = '男';
-
+    $john->walk();
     var_dump($john);
     
     $may = new Role;
     $may -> name = 'May';
     $may -> gender = '女';
+    $may -> walk();
     var_dump($may);
