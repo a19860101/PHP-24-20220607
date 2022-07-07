850268c76ead95bf9147640770499dea0791703c
Zac <a19860101@gmail.com>
2022-07-07 21:01:02 +0800

    20220707

--- blog/public/template/header.php
+++ blog/public/template/header.php
@@ -12,15 +12,35 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
-    <nav>
-        <a href="<?php echo $webroot; ?>" class="logo">Logo</a>
-        <div>
-            <a href="<?php echo $webroot; ?>/post/create.php">建立文章</a>
-            <a href="<?php echo $webroot; ?>/category/index.php">分類管理</a>
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark mb-5">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?php echo $webroot; ?>">LOGO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="<?php echo $webroot; ?>">文章列表</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo $webroot; ?>/post/create.php" class="nav-link">建立文章</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo $webroot; ?>/category/index.php" class="nav-link">分類管理</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">註冊</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">登入</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">登出</a>
+                </li>
+            </ul>
         </div>
-        <div>
-            <a href="#">註冊</a>
-            <a href="#">登入</a>
-            <a href="#">登出</a>
-        </div>
-    </nav>
+    </div>
+</nav>
