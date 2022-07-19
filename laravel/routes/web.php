8ac50b52dee60912df1a0056e10dfecc28f6ad2d
Zac <a19860101@gmail.com>
2022-07-19 19:32:10 +0800

    20220719

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,16 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\PostController;
+
+
 Route::get('/', function(){
     return 'Index';
 });
@@ -21,3 +14,5 @@ Route::get('/welcome', function () {
 Route::get('/about', function () {
     return view('about');
 });
+Route::get('/test',[App\Http\Controllers\PostController::class,'test']);
+Route::get('/hello/about',[PostController::class,'hello']);
