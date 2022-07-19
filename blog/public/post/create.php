f4b92b347b5464a8d2d70e2425815c12eec4c027
Zac <a19860101@gmail.com>
2022-07-19 19:08:21 +0800

    20220719

--- blog/public/post/create.php
+++ blog/public/post/create.php
@@ -39,5 +39,49 @@
 
 
 <?php include('../template/footer.php'); ?>
-<?php include('../template/tinymce.php'); ?>
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    tinymce.init({
+        selector: 'textarea',
+        language: 'zh_TW',
+        height: 600,
+        menubar: 'file edit view insert format tools table help',
+        plugins:'image',
+        toolbar: 'image',
+        images_upload_base_path: 'postAcceptor.php',
+        image_title: false,
+        file_picker_types: 'image',
+        image_uploadtab: false,
+        automatic_uploads: true,
+        file_picker_callback: (cb, value, meta) => {
+            const input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+
+            input.addEventListener('change', (e) => {
+                const file = e.target.files[0];
+            
+                const reader = new FileReader();
+                reader.addEventListener('load', () => {
+                    /*
+                    Note: Now we need to register the blob in TinyMCEs image blob
+                    registry. In the next release this part hopefully won't be
+                    necessary, as we are looking to handle it internally.
+                    */
+                    const id = 'blobid' + (new Date()).getTime();
+                    const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                    const base64 = reader.result.split(',')[1];
+                    const blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+                    /* call the callback and populate the Title field with the file name */
+                    cb(blobInfo.blobUri(), { title: file.name });
+                });
+                reader.readAsDataURL(file);
+                console.log(file.name);
+            });
+
+            input.click();
+        },
+    });
+</script>
 
