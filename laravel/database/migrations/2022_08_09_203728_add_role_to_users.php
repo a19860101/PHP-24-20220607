0e60cc2516e035a4900b376a2fc719cae600c421
Zac <a19860101@gmail.com>
2022-08-09 20:40:14 +0800

    20220809

---
+++ laravel/database/migrations/2022_08_09_203728_add_role_to_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRoleToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            //  0 -> 管理員 1 -> 一般會員
+            $table->string('role')->default('1');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('role');
+        });
+    }
+}
