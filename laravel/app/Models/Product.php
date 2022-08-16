26fe0ec553902955f93aaab976a6d7ae4691402d
Zac <a19860101@gmail.com>
2022-08-16 19:13:27 +0800

    20220816

--- laravel/app/Models/Product.php
+++ laravel/app/Models/Product.php
@@ -15,4 +15,7 @@ class Product extends Model
     public function category(){
         return $this->belongsTo(Category::class);
     }
+    public function carts(){
+        return $this->hasMany(Cart::class);
+    }
 }
--- laravel/resources/views/cart/list.blade.php
+++ laravel/resources/views/cart/list.blade.php
@@ -11,7 +11,17 @@
         </div>
         @foreach($carts as $cart)
         <div class="col-8">
-            <h2>{{$cart->product_id}}</h2>
+            <div class="border rounded-3 my-2 p-4">
+                <h4>{{$cart->product->title}}</h4>
+                <div>
+                    @if($cart->product->sale)
+                    原價<del>{{$cart->product->price}}</del>
+                    <b class="text-danger">特價{{$cart->product->sale}}</b>
+                    @else
+                    <b>售價{{$cart->product->price}}</b>
+                    @endif
+                </div>
+            </div>
         </div>
         @endforeach
     </div>
